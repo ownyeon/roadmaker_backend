d567f3bd97921a7d6b5c05dc850a15afee0a8ef8
jongbeom <postrich2020@gmail.com>
2025-02-20 17:11:42 +0900

    250220_1711_구조분류

---
+++ src/main/java/com/roadmaker/c_kimjongbeom/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.roadmaker.c_kimjongbeom.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AuthController {
+
+    //필드 주입, 변경 가능
+}
